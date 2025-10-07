63831bc267339d0539eaa9a1dbc4eeb7ed8ce73e
stykmorfem <theather206@gmail.com>
2025-10-08 01:41:47 +0300

    Update solution.java

--- solution.java
+++ solution.java
@@ -1,1 +1,33 @@
-// Solution for Homework 3.3
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        String oper = input.nextLine();
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+
+        switch (oper) {
+            case "Сумма":
+                System.out.println(name + " ваш результат: " + (num1 + num2));
+                break;
+            case "Вычитание":
+                System.out.println(name + " ваш результат: " + (num1 - num2));
+                break;
+            case "Умножение":
+                System.out.println(name + " ваш результат: " + (num1 * num2));
+                break;
+            case "Деление":
+                if (num2 == 0) {
+                    System.out.println("На ноль делить нельзя!");
+                    break;
+                }
+                System.out.println(name + " ваш результат: " + (num1 / num2));
+                break;
+            default:
+                System.out.println("Неправильно введен оператор");
+        }
+    }
+}
